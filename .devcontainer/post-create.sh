963afaa56252ac32c9d1b78d143827430c0a72f1
Takahisa HAMAJI <hamaji_takahisa@kurusugawa.jp>
2022-01-28 19:56:04 +0900

    少しシンプルになった

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# go1.18beta1
-go install golang.org/dl/go1.18beta1@latest
-go1.18beta1 download
-echo 'alias go=go1.18beta1' >> /home/vscode/.bashrc
-
 # gopls
 # https://github.com/golang/tools/blob/master/gopls/doc/advanced.md#working-with-generic-code
 cd $(mktemp -d)
-go1.18beta1 mod init gopls-unstable
-go1.18beta1 get golang.org/x/tools/gopls@master golang.org/x/tools@master
-go1.18beta1 install golang.org/x/tools/gopls
+go mod init gopls-unstable
+go get golang.org/x/tools/gopls@master golang.org/x/tools@master
+go install golang.org/x/tools/gopls
